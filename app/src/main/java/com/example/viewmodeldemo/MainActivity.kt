87f2dc6a6e04896ff3445f14e1f202d1092b17ee
lakpahana <agslakpahana@gmail.com>
2022-08-13 15:08:45 +0530

    viewModel

---
+++ app/src/main/java/com/example/viewmodeldemo/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.viewmodeldemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+
+class MainActivity : AppCompatActivity() {
+    var totalCount = 0
+    private lateinit var viewModel: MainActivityViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        //var totalCount = 0
+        val countButton = findViewById<Button>(R.id.btnCount)
+        val countTextView  = findViewById<TextView>(R.id.tvCount)
+        countTextView.text = viewModel.count.toString()
+        countButton.setOnClickListener {
+            //totalCount += 1
+        //    ++totalCount
+//            countTextView.text = totalCount.toString()
+            viewModel.updateCount()
+        countTextView.text = viewModel.count.toString()
+        }
+
+    }
+
+
+}
